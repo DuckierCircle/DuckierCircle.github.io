ce7e40ec7e5fea6a72277b8f9d5ec0c5b43d83ec
Bryce <bzhabel@gmail.com>
2023-02-26 07:13:25 -0700

    changes

--- html/wallpaperScript.js
+++ html/wallpaperScript.js
@@ -2,6 +2,5 @@
 function randomWallpaper() {
     let arrayOfImages = [];
     x = Math.floor(Math.random() * 13) + 1;
-    let src = 'wbf_wallpapers/water_buffalo' + x.toString();
-    return src
+    let src = 'wbf_wallpapers/water_buffalo' + x.toString() + '.jpg';
 };
