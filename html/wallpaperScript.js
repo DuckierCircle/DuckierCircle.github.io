d36e83754e20c9b763a8d592610fdf8f5e294c08
Bryce <bzhabel@gmail.com>
2023-02-26 07:30:04 -0700

    folder reorganization

--- html/wallpaperScript.js
+++ html/wallpaperScript.js
@@ -2,5 +2,5 @@
 function randomWallpaper() {
     let arrayOfImages = [];
     x = Math.floor(Math.random() * 13) + 1;
-    let src = 'wbf_wallpapers/water_buffalo' + x.toString() + '.jpg';
+    let src = 'images/wbf_wallpapers/water_buffalo' + x.toString() + '.jpg';
 };
