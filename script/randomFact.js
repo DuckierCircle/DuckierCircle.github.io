8338ad2008967f044b550d66317f1eab3a5f6a0d
Bryce <bzhabel@gmail.com>
2023-03-07 20:21:30 -0700

    wallpapers and randomFacts no longer repeat. used a textPrevious variable and if statement to check each new image or fact to make sure it was not the same as the last one

--- script/randomFact.js
+++ script/randomFact.js
@@ -1,37 +1,90 @@
 // JavaScript source code
+
+let text = '';
+let textPrevious = '';
+
 function randomFact() {
     let x = Math.floor(Math.random() * 10);
-    let text = '';
     switch (x) {
         case 0:
             text = "Water Buffaloes Live on Every Continent Except Antarctica";
+            if (text === textPrevious) {
+                randomFact();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 1:
             text = "Different Subspecies of Water Buffaloes Live in Different Areas";
+            if (text === textPrevious) {
+                randomFact();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 2:
             text = "Water Buffaloes Weigh More Than Two Grizzly Bears";
+            if (text === textPrevious) {
+                randomFact();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 3:
             text = "Water Buffaloes Love the Water, Hence Their Name";
+            if (text === textPrevious) {
+                randomFact();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 4:
             text = "Water Buffaloes Are Herbivores";
+            if (text === textPrevious) {
+                randomFact();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 5:
             text = "Domesticated Water Buffaloes Threaten Wild Water Buffaloes";
+            if (text === textPrevious) {
+                randomFact();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 6:
             text = "Water Buffaloes Are Pregnant for a Bit Longer Than People";
+            if (text === textPrevious) {
+                randomFact();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 7:
             text = "Wild and Domestic Water Buffalo Population Differences Are Enormous";
+            if (text === textPrevious) {
+                randomFact();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 8:
             text = "People Love Water Buffalo Milk";
+            if (text === textPrevious) {
+                randomFact();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 9:
             text = "Brazilian Police Use Water Buffaloes";
+            if (text === textPrevious) {
+                randomFact();
+            } else {
+                textPrevious = text;
+            }
             break;
     }
     document.getElementById("output").innerHTML = text;
--- script/wallpaperScript.js
+++ script/wallpaperScript.js
@@ -1,46 +1,114 @@
 // JavaScript source code
+
+let text = '';
+let textPrevious = '';
+
 function randomWallpaper() {
     let x = Math.floor(Math.random() * 10);
-    let text = '';
     switch (x) {
         case 0:
             text = "images/wbf_wallpapers/water_buffalo1.jpg";
+            if (text === textPrevious) {
+                randomWallpaper();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 1:
             text = "images/wbf_wallpapers/water_buffalo2.jpeg";
+            if (text === textPrevious) {
+                randomWallpaper();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 2:
             text = "images/wbf_wallpapers/water_buffalo3.jpg";
+            if (text === textPrevious) {
+                randomWallpaper();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 3:
             text = "images/wbf_wallpapers/water_buffalo4.jpeg";
+            if (text === textPrevious) {
+                randomWallpaper();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 4:
             text = "images/wbf_wallpapers/water_buffalo5.jpg";
+            if (text === textPrevious) {
+                randomWallpaper();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 5:
             text = "images/wbf_wallpapers/water_buffalo6.jpg";
+            if (text === textPrevious) {
+                randomWallpaper();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 6:
             text = "images/wbf_wallpapers/water_buffalo7.jpg";
+            if (text === textPrevious) {
+                randomWallpaper();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 7:
             text = "images/wbf_wallpapers/water_buffalo8.jpeg";
+            if (text === textPrevious) {
+                randomWallpaper();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 8:
             text = "images/wbf_wallpapers/water_buffalo9.jpeg";
+            if (text === textPrevious) {
+                randomWallpaper();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 9:
             text = "images/wbf_wallpapers/water_buffalo10.jpeg";
+            if (text === textPrevious) {
+                randomWallpaper();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 10:
             text = "images/wbf_wallpapers/water_buffalo11.jpeg";
+            if (text === textPrevious) {
+                randomWallpaper();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 11:
             text = "images/wbf_wallpapers/water_buffalo12.jpeg";
+            if (text === textPrevious) {
+                randomWallpaper();
+            } else {
+                textPrevious = text;
+            }
             break;
         case 12:
             text = "images/wbf_wallpapers/water_buffalo13.jpg";
+            if (text === textPrevious) {
+                randomWallpaper();
+            } else {
+                textPrevious = text;
+            }
             break;
     }
     document.getElementById("wallpaper").src = text;
