a55085f697c564c95afa56dba497d7c013c49501
Bryce <bzhabel@gmail.com>
2023-03-12 20:13:11 -0600

    In javascript code for randomFact, changed the textPrevious variable to textCurrent as this seemed more appropriate since the function is looking at the same text that is currently being displayed onscreen.

--- script/wallpaperScript.js
+++ script/wallpaperScript.js
@@ -8,106 +8,106 @@ function randomWallpaper() {
     switch (x) {
         case 0:
             text = "images/wbf_wallpapers/water_buffalo1.jpg";
-            if (text === textPrevious) {
+            if (text === textCurrent) {
                 randomWallpaper();
             } else {
-                textPrevious = text;
+                textCurrent = text;
             }
             break;
         case 1:
             text = "images/wbf_wallpapers/water_buffalo2.jpeg";
-            if (text === textPrevious) {
+            if (text === textCurrent) {
                 randomWallpaper();
             } else {
-                textPrevious = text;
+                textCurrent = text;
             }
             break;
         case 2:
             text = "images/wbf_wallpapers/water_buffalo3.jpg";
-            if (text === textPrevious) {
+            if (text === textCurrent) {
                 randomWallpaper();
             } else {
-                textPrevious = text;
+                textCurrent = text;
             }
             break;
         case 3:
             text = "images/wbf_wallpapers/water_buffalo4.jpeg";
-            if (text === textPrevious) {
+            if (text === textCurrent) {
                 randomWallpaper();
             } else {
-                textPrevious = text;
+                textCurrent = text;
             }
             break;
         case 4:
             text = "images/wbf_wallpapers/water_buffalo5.jpg";
-            if (text === textPrevious) {
+            if (text === textCurrent) {
                 randomWallpaper();
             } else {
-                textPrevious = text;
+                textCurrent = text;
             }
             break;
         case 5:
             text = "images/wbf_wallpapers/water_buffalo6.jpg";
-            if (text === textPrevious) {
+            if (text === textCurrent) {
                 randomWallpaper();
             } else {
-                textPrevious = text;
+                textCurrent = text;
             }
             break;
         case 6:
             text = "images/wbf_wallpapers/water_buffalo7.jpg";
-            if (text === textPrevious) {
+            if (text === textCurrent) {
                 randomWallpaper();
             } else {
-                textPrevious = text;
+                textCurrent = text;
             }
             break;
         case 7:
             text = "images/wbf_wallpapers/water_buffalo8.jpeg";
-            if (text === textPrevious) {
+            if (text === textCurrent) {
                 randomWallpaper();
             } else {
-                textPrevious = text;
+                textCurrent = text;
             }
             break;
         case 8:
             text = "images/wbf_wallpapers/water_buffalo9.jpeg";
-            if (text === textPrevious) {
+            if (text === textCurrent) {
                 randomWallpaper();
             } else {
-                textPrevious = text;
+                textCurrent = text;
             }
             break;
         case 9:
             text = "images/wbf_wallpapers/water_buffalo10.jpeg";
-            if (text === textPrevious) {
+            if (text === textCurrent) {
                 randomWallpaper();
             } else {
-                textPrevious = text;
+                textCurrent = text;
             }
             break;
         case 10:
             text = "images/wbf_wallpapers/water_buffalo11.jpeg";
-            if (text === textPrevious) {
+            if (text === textCurrent) {
                 randomWallpaper();
             } else {
-                textPrevious = text;
+                textCurrent = text;
             }
             break;
         case 11:
             text = "images/wbf_wallpapers/water_buffalo12.jpeg";
-            if (text === textPrevious) {
+            if (text === textCurrent) {
                 randomWallpaper();
             } else {
-                textPrevious = text;
+                textCurrent = text;
             }
             break;
         case 12:
             text = "images/wbf_wallpapers/water_buffalo13.jpg";
-            if (text === textPrevious) {
+            if (text === textCurrent) {
                 randomWallpaper();
             } else {
-                textPrevious = text;
+                textCurrent = text;
             }
             break;
     }
