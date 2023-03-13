f134e7da1fdd72234fa0f2dd28167b609745fff2
Bryce <bzhabel@gmail.com>
2023-03-13 05:11:25 -0600

    much simpler and more effective animation on hover over navigation items. using border now instead of multiple lines of code that were an overcomplication method for performing the same task

--- script/navbar.js
+++ script/navbar.js
@@ -1,4 +1,4 @@
 // JavaScript source code
 function navbar() {
-    document.getElementById("navbar").innerHTML = "<div class='navbar'><nav><ul><li><a href='https://waterbuffalofacts.com'>Home</a></li><li><a href='contact.html'>Contact Us</a></li><li><a href='randomfact.html'>Random Fact</a></li><li><a href='wallpapers.html'>Wallpapers</a></li></ul></nav><a href='https://waterbuffalofacts.com'><img style='float: left;'src='images/wbf_logo.png'width='120'height='120'alt='Water Buffalo Facts Logo'class='logo'/></a></div>"
+    document.getElementById("navbar").innerHTML = "<div class='navbar'><nav><ul><li><a href='https://waterbuffalofacts.com'>Home</a></li><li><a href='randomfact.html'>Random Fact</a></li><li><a href='wallpapers.html'>Wallpapers</a></li><li><a href='contact.html'>Contact Us</a></li></ul></nav><a href='https://waterbuffalofacts.com'><img style='float: left;'src='images/wbf_logo.png'width='120'height='120'alt='Water Buffalo Facts Logo'class='logo'/></a></div>"
 }
