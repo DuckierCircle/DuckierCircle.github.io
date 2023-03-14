6530461f179a1a6f92aec51a7594bd2d1c4b74f8
Bryce <bzhabel@gmail.com>
2023-03-14 07:33:27 -0600

    dropdown for mobile navbar

--- script/navbar.js
+++ script/navbar.js
@@ -1,4 +1,13 @@
 // JavaScript source code
 function navbar() {
-    document.getElementById("navbar-container").innerHTML = "<div class='navbar'><nav><ul><li><a href='https://waterbuffalofacts.com'>Home</a></li><li><a href='randomfact.html'>Random Fact</a></li><li><a href='wallpapers.html'>Wallpapers</a></li><li><a href='contact.html'>Contact Us</a></li></ul></nav><a href='https://waterbuffalofacts.com'><img style='float: left;'src='images/wbf_logo.png'width='120'height='120'alt='Water Buffalo Facts Logo'class='logo'/></a></div>"
+    document.getElementById("navbar-container").innerHTML = "<div class='navbar'><nav><div class='hamburger-container' onclick='navMenu()'><div class='hamburger'></div><div class='hamburger'></div><div class='hamburger'></div></div><ul id='nav-menu'><li><a href='https://waterbuffalofacts.com'>Home</a></li><li><a href='randomfact.html'>Random Fact</a></li><li><a href='wallpapers.html'>Wallpapers</a></li><li><a href='contact.html'>Contact Us</a></li></ul></nav><a href='https://waterbuffalofacts.com'><img style='float: left;'src='images/wbf_logo.png'width='120'height='120'alt='Water Buffalo Facts Logo'class='logo'/></a></div>"
+}
+
+
+function navMenu() {
+    if (document.getElementById('nav-menu').style.margin === '-100%') {
+        document.getElementById('nav-menu').style.margin = '0px';
+    } else {
+        document.getElementById('nav-menu').style.margin = '-100%'
+    }
 }
